6d2dcace548942e73b0639199b74309bee0b8457
vagrant <vagrant@precise32.(none)>
2014-02-07 02:42:48 +0000

    fixed some stuff

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,42 @@
 /*
  * Function that sets up the home screen.
  */
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("Javascript connected!");
+
+	$("#start-start-button").on("click", function(e) {
+		console.log("CLICK START");
+		$("#start-page").css("display", "none");
+		$("#active-page").css("display", "inline");
+	});
+
+	$("#active-finish-button").on("click", function(e) {
+
+		$("#active-page").css("display", "none");
+		$("#results-page").css("display", "inline");
+	});
+
+	$("#results-home-button").on("click", function(e) {
+
+		$("#results-page").css("display", "none");
+		$("#start-page").css("display", "inline");
+	});
+
+	initHomeScreen();
+
+}
+
+ 
 function initHomeScreen() {
 	$("#introBox").hide();
 	$("#preCanvas").click(showInstructions);
