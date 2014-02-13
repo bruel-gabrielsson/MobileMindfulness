1704335f69ab10d2e3d99fa04e17e4708ed7a852
Tomas Forsyth Rosin <tomasrosin@gmail.com>
2014-02-13 04:59:53 +0000

    fixed problems with condensed view after sessions shorter than time limit

--- public/js/breathResults.js
+++ public/js/breathResults.js
@@ -53,59 +53,70 @@ BreathResults.prototype.populate = function(data) {
 
 		// Draw full graph
 		var ctx = this.fullGraph.getContext('2d');
-
-		ctx.strokeStyle = color;
-		ctx.lineWidth = lineWidth;
-		ctx.clearRect(0,0,fullWidth,canvasHeight);
-		ctx.beginPath();
-		var firstPoint = data[0],
-			t0 = firstPoint.t;
-		ctx.moveTo(0,(0.9 - firstPoint.y*0.8)*canvasHeight);
-		var point;
-		for (var i = 1; i < length; i++) {
-			point = data[i];
-			ctx.lineTo((point.t-t0)/timeSpan*fullWidth,(0.9 -point.y*0.8)*canvasHeight);
-		}
-		ctx.stroke();
-		ctx.closePath();
-
+		this.drawFullGraph(ctx, data, fullWidth, canvasHeight, timeSpan);
 
 		// Draw condensed graph
 		ctx = this.condensed.getContext('2d');
-		ctx.strokeStyle = color;
-		ctx.lineWidth = lineWidth;
-		ctx.clearRect(0,0,canvasWidth,canvasHeight);
-
-		// Draw first sequence
-		ctx.beginPath();
-		var firstPoint = data[0],
-			t0 = firstPoint.t;
-		ctx.moveTo(0,(0.9 - firstPoint.y*0.8)*canvasHeight);
-		var point;
-		for (var i = 1; i < length; i++) {
-			point = data[i];
-			var part = (point.t-t0)/timeSpan,
-				x = Math.min(part*fullWidth,0.5*canvasWidth);
-			ctx.lineTo(x,(0.9 -point.y*0.8)*canvasHeight);
-			if (part/ratio > 0.5) break;
+		if (ratio < 1) {
+			this.drawFullGraph(ctx, data, fullWidth, canvasHeight, timeSpan);
+		} else {
+			this.drawCondensedGraph(ctx, data, ratio, fullWidth, canvasWidth, canvasHeight, timeSpan);
 		}
-		ctx.stroke();
-		ctx.closePath();
-
-		// Draw last sequence
-		ctx.beginPath();
-		var lastPoint = data[length-1],
-			tl = lastPoint.t;
-		ctx.moveTo(canvasWidth,(0.9 - lastPoint.y*0.8)*canvasHeight);
-		var point;
-		for (var i = length-2; i >= 0; i--) {
-			point = data[i];
-			var part = (tl-point.t)/timeSpan,
-				x = Math.max(canvasWidth-part*fullWidth,0.5*canvasWidth);
-			ctx.lineTo(x,(0.9 -point.y*0.8)*canvasHeight);
-			if (part/ratio > 0.5) break;
-		}
-		ctx.stroke();
-		ctx.closePath();
 	}
 };
+
+BreathResults.prototype.drawFullGraph = function(ctx, data, fullWidth, canvasHeight, timeSpan) {
+	var length = data.length;
+	ctx.strokeStyle = this.color;
+	ctx.lineWidth = this.lineWidth;
+	ctx.clearRect(0,0,fullWidth,canvasHeight);
+	ctx.beginPath();
+	var firstPoint = data[0],
+		t0 = firstPoint.t;
+	ctx.moveTo(0,(0.9 - firstPoint.y*0.8)*canvasHeight);
+	var point;
+	for (var i = 1; i < length; i++) {
+		point = data[i];
+		ctx.lineTo((point.t-t0)/timeSpan*fullWidth,(0.9 -point.y*0.8)*canvasHeight);
+	}
+	ctx.stroke();
+	ctx.closePath();
+}
+
+BreathResults.prototype.drawCondensedGraph = function(ctx, data, ratio, fullWidth, canvasWidth, canvasHeight, timeSpan) {
+	var length = data.length;
+	ctx.strokeStyle = this.color;
+	ctx.lineWidth = this.lineWidth;
+	ctx.clearRect(0,0,canvasWidth,canvasHeight);
+	// Draw first sequence
+	ctx.beginPath();
+	var firstPoint = data[0],
+		t0 = firstPoint.t;
+	ctx.moveTo(0,(0.9 - firstPoint.y*0.8)*canvasHeight);
+	var point;
+	for (var i = 1; i < length; i++) {
+		point = data[i];
+		var part = (point.t-t0)/timeSpan,
+			x = Math.min(part*fullWidth,0.5*canvasWidth);
+		ctx.lineTo(x,(0.9 -point.y*0.8)*canvasHeight);
+		if (part/ratio > 0.5) break;
+	}
+	ctx.stroke();
+	ctx.closePath();
+
+	// Draw last sequence
+	ctx.beginPath();
+	var lastPoint = data[length-1],
+		tl = lastPoint.t;
+	ctx.moveTo(canvasWidth,(0.9 - lastPoint.y*0.8)*canvasHeight);
+	var point;
+	for (var i = length-2; i >= 0; i--) {
+		point = data[i];
+		var part = (tl-point.t)/timeSpan,
+			x = Math.max(canvasWidth-part*fullWidth,0.5*canvasWidth);
+		ctx.lineTo(x,(0.9 -point.y*0.8)*canvasHeight);
+		if (part/ratio > 0.5) break;
+	}
+	ctx.stroke();
+	ctx.closePath();
+}
