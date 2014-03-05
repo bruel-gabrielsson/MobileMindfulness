dec062e9a6b0b3052398be2c3ec56a905429a7b3
Mathan Glezer <mathan.glezer@gmail.com>
2014-03-05 23:43:22 +0000

    Ensures data gets saved EVERY time, regardless of whether user is logged in.

--- routes/breathingsession.js
+++ routes/breathingsession.js
@@ -16,6 +16,14 @@ exports.save = function(User) {
 	return function(req, res) {
     	var form_data = req.body;
 
+      var newBreathingsession = new models.BreathingSession({
+              "date": new Date(),
+              "data": form_data.data,
+              "username": "TESTING",
+              "guidance": form_data.guidance
+            });
+      newBreathingsession.save(function(err) {if (err) {console.log(err)}});
+
 	    var username = req.session.username;
 	    if (username) {
 	    	User.find({ name: username }, function(error, result) {
@@ -29,8 +37,7 @@ exports.save = function(User) {
               "date": new Date(),
               "data": form_data.data,
               "_user": usr.id,
-              "username": usr.name, 
-              "guidance": form_data.guidance
+              "username": usr.name
             });
             newBreathingsession.save(function(err) {if (err) {console.log(err)}});
 
@@ -38,14 +45,6 @@ exports.save = function(User) {
 		      }
 		    });
 	    } else {
-        var newBreathingsession = new models.BreathingSession({
-                "date": new Date(),
-                "data": form_data.data,
-                "username": "TESTING",
-                "guidance": form_data.guidance
-              });
-        
-              newBreathingsession.save(function(err) {if (err) {console.log(err)}});
         res.json({message:'You have to be logged in to save your results.</br><a href="./">Register here!</a>'});
       }
 	};
